3bea7fa6be98c35b7e8e29e2050dd12c08c17329
vinialma <65692589+vinialma@users.noreply.github.com>
2021-05-02 17:23:56 -0300

    Criação do sistema de troca :sparkles:
    Criando o sistema de troca e fazendo melhorias no mecanismo de batalha.

--- N1/src/main/java/uam/n1/Main.java
+++ N1/src/main/java/uam/n1/Main.java
@@ -8,17 +8,17 @@ public class Main {
         int escolha = 0, i = 0;
         Elemonstro[] jogador1 = new Elemonstro[3];
         Elemonstro[] jogador2 = new Elemonstro[3];
+        Scanner entrada = new Scanner(System.in);
         for(int j=0 ; j<6; j++){
             System.out.println("Jogador "+ (i+1) + "! Escolha 3 Elemonstros!");
             System.out.println("---------------------------------");
             System.out.println("| 1 - Draterra\n| 2 - Ventáguia\n| 3 - Zillétrico\n| 4 - Águarto\n|"
                     + " 5 - Sementauro\n| 6 - Lobrasa\n|__________________________________");
-            Scanner entrada = new Scanner(System.in);
             escolha = Integer.parseInt(entrada.nextLine());
             if(i == 0)
-                elemonstro(jogador1,escolha,i,j);
+                selecao(jogador1,escolha,i,j);
             else
-                elemonstro(jogador2,escolha,i,j);
+                selecao(jogador2,escolha,i,j);
             if(j==2)
                 i++;
         }
@@ -33,35 +33,75 @@ public class Main {
             System.out.println("O Jogador não pode escolher dois monstros iguais!");
             System.exit(0);
         }
-        int[] escolhaataque;
-        escolhaataque = new int [2];
-        escolhaataque[0]=0;
-        escolhaataque[1]=0;
-        Scanner entrada = new Scanner(System.in);
-        do {
-        	for( i=0;i<2;i++) {
-        		System.out.println("---------------------");
-	        	if(i==0) {
-	        		System.out.println("jogador "+ (i+1));
-	        		System.out.println("Você está com o "+jogador1[0].getNomeElemonstro());
-	        		System.out.println("ataque 1 : "+jogador1[0].getNomeGolpe1());
-	        		System.out.println("ataque 2 : "+jogador1[0].getNomeGolpe2());
-	        		System.out.println("Escolha o ataque (1 ou 2)");
-	        		escolhaataque[0] = entrada.nextInt();
-	            }else {
-	            	System.out.println("jogador "+ (i+1));
-	        		System.out.println("Você está com o "+jogador2[0].getNomeElemonstro());
-	        		System.out.println("ataque 1 : "+jogador2[0].getNomeGolpe1());
-	        		System.out.println("ataque 1 : "+jogador2[0].getNomeGolpe2());
-	        		System.out.println("Escolha o ataque (1 ou 2)");
-	        		escolhaataque[1] = entrada.nextInt();
-	            }
-        	}
-        	
-        }while((escolhaataque[0] < 1 || escolhaataque[0] >2) && (escolhaataque[1] < 1 || escolhaataque[1] >2));
+        int nocaute1 = 0, nocaute2=0;
+        int[] escolhaAtaque;
+        int elemonstroAtual1=0, elemonstroAtual2=0;
+        int escolherAcao = 0;
+        escolhaAtaque = new int [2];
+        escolhaAtaque[0]=0;
+        escolhaAtaque[1]=0;
+        while(nocaute1<3 || nocaute2<3){
+            for(i=0;i<2;i++) {
+                if(jogador1[elemonstroAtual1].getVida() > 0
+                        || jogador2[elemonstroAtual2].getVida() > 0){
+                    System.out.println("---------------------");
+                    System.out.println("Jogador "+(i+1)+" escolha sua ação: ");
+                    System.out.println("\t1 -> Atacar\n\t2 -> Trocar");
+                    escolherAcao = entrada.nextInt();
+                }else{
+                    escolherAcao = 2;
+                }
+                switch(escolherAcao){
+                    case 1: do {
+                                System.out.println("---------------------");
+                                if(i==0) {
+                                    System.out.println("Jogador "+ (i+1));
+                                    System.out.println("Você está com o "+jogador1[elemonstroAtual1].getNomeElemonstro());
+                                    System.out.println("Ataque 1 : "+jogador1[elemonstroAtual1].getNomeGolpe1());
+                                    System.out.println("Ataque 2 : "+jogador1[elemonstroAtual1].getNomeGolpe2());
+                                    System.out.println("Escolha o ataque (1 ou 2)");
+                                    escolhaAtaque[0] = entrada.nextInt();
+                                }else {
+                                    System.out.println("Jogador "+ (i+1));
+                                    System.out.println("Você está com o "+jogador2[elemonstroAtual2].getNomeElemonstro());
+                                    System.out.println("Ataque 1 : "+jogador2[elemonstroAtual2].getNomeGolpe1());
+                                    System.out.println("Ataque 2 : "+jogador2[elemonstroAtual2].getNomeGolpe2());
+                                    System.out.println("Escolha o ataque (1 ou 2)");
+                                    escolhaAtaque[1] = entrada.nextInt();
+                                }
+                            }while((escolhaAtaque[0] < 1 || escolhaAtaque[0] >2) && (escolhaAtaque[1] < 1 || escolhaAtaque[1] >2));
+                            break;
+                    case 2: System.out.println("Para qual Elemonstro deseja trocar?");
+                            if(i==0){
+                                do{
+                                    System.out.println("1 -> "+jogador1[0].getNomeElemonstro()+
+                                            "\n2 -> "+jogador1[1].getNomeElemonstro()+
+                                            "\n3 -> "+jogador1[2].getNomeElemonstro());
+                                    elemonstroAtual1 = (entrada.nextInt()-1);
+                                    if(jogador1[elemonstroAtual1].getVida()<=0){
+                                        System.out.println("Esse elemonstro está nocauteado!");
+                                        elemonstroAtual1 = 5;
+                                    }
+                                }while(elemonstroAtual1==5);
+                            }else{
+                                do{
+                                    System.out.println("1 -> "+jogador2[0].getNomeElemonstro()+
+                                            "\n2 -> "+jogador2[1].getNomeElemonstro()+
+                                            "\n3 -> "+jogador2[2].getNomeElemonstro());
+                                    elemonstroAtual2 = (entrada.nextInt()-1);
+                                    if(jogador2[elemonstroAtual2].getVida()<=0){
+                                        System.out.println("Esse elemonstro está nocauteado!");
+                                        elemonstroAtual2 = 5;
+                                    }
+                                }while(elemonstroAtual2==5);
+                            }
+                            break;
+                }
+            }
+        }
     }
     
-    public static void elemonstro(Elemonstro[] jogador, int escolha, int i, int j){
+    public static void selecao(Elemonstro[] jogador, int escolha, int i, int j){
         if(i == 0){
             Elemonstro draterra1 = new Elemonstro("Terremoto","Garras Granito",70,55,"Terra","Draterra","Terra",140,150,160,80);
             Elemonstro ventaguia1 = new Elemonstro("Vendaval","Bico Mortal",40,60,"Vento","Ventaguia","Vento",200,170,80,110);
@@ -95,6 +135,4 @@ public class Main {
         }
         
     }
-    
-    
 }
